5552b20d385a73f0a56ade4b0921da00ce3c3eea
AnaLTFernandes <analuizateixeirafernandes184@gmal.com>
2022-06-15 22:34:53 -0300

    Virar cartas concluído

--- scripts.js
+++ scripts.js
@@ -1,17 +1,23 @@
 
 setTimeout(() => {
-    
-let qdtCartas = prompt("Com quantas cartas deseja jogar?");
-let contador = 0;
+        
+    let qdtCartas = prompt("Com quantas cartas deseja jogar?");
 
-while ((qdtCartas % 2) !== 0 || qdtCartas < 4 || qdtCartas > 14) {
-    qdtCartas = prompt("Com quantas cartas deseja jogar?");
+    while ((qdtCartas % 2) !== 0 || qdtCartas < 4 || qdtCartas > 14) {
+        qdtCartas = prompt("Com quantas cartas deseja jogar?");
+    }
+
+    for (let i = 0; i < qdtCartas; i++) {
+        document.querySelector(".mesa").innerHTML += `
+        <div class="card back" onclick="virarCard(this)">
+            <img src="./img/back-face.png">
+        </div>`;
+    }
+}, 1500);
+
+
+function virarCard(carta) {
+    carta.classList.toggle("virar");
+    carta.classList.toggle("back");
+    carta.classList.toggle("front");
 }
-
-while (contador < qdtCartas) {
-    contador ++;
-    document.querySelector(".mesa").innerHTML += `
-    <div class="card back">
-        <img src="./img/back-face.png">
-    </div>`;
-}}, 2000);
