68572998e4d5df992004a772d497358520f01def
AnaLTFernandes <analuizateixeirafernandes184@gmal.com>
2022-06-16 13:41:52 -0300

    Embaralhar e distribuição de cartas aleatórias

--- scripts.js
+++ scripts.js
@@ -1,12 +1,37 @@
+let qdtCartas;
+let listaCards = [1,2,3,4,5,6,7];
+let baralho = [];
+
 
 setTimeout(() => {
         
-    let qdtCartas = prompt("Com quantas cartas deseja jogar?");
+    qdtCartas = prompt("Com quantas cartas deseja jogar? (4-14)");
 
     while ((qdtCartas % 2) !== 0 || qdtCartas < 4 || qdtCartas > 14) {
         qdtCartas = prompt("Com quantas cartas deseja jogar?");
     }
 
+    atualizarBaralho()
+}, 1500);
+
+
+function atualizarBaralho() {
+    listaCards.sort(embaralhar);
+
+    for (let i = 0; i < (qdtCartas/2); i++) {
+        baralho.push(listaCards[i]);
+    }
+    for (let i = 0; i < (qdtCartas/2); i++) {
+        baralho.push(listaCards[i]);
+    }
+    
+    baralho.sort(embaralhar);
+
+    colocarCartas();
+}
+
+
+function colocarCartas() {
     for (let i = 0; i < qdtCartas; i++) {
         document.querySelector(".mesa").innerHTML += `
         <div class="card" onclick="virarCard(this)">
@@ -14,13 +39,18 @@ setTimeout(() => {
                 <img src="./img/back-face.png">
             </div>
             <div class="front-face face">
-                <img src="./img/front-face/bobrossparrot.gif">
+                <img src="./img/front-face/front-card-${baralho[i]}.gif">
             </div>
         </div>`;
     }
-}, 1500);
+}
 
 
 function virarCard(carta) {
     carta.classList.toggle("virar");
+}
+
+
+function embaralhar() {
+    return Math.random() - 0.5;
 }
