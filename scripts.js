65311fa1d5b7e7ef0e44cbeb22963101fbfab5d1
AnaLTFernandes <analuizateixeirafernandes184@gmal.com>
2022-06-18 17:32:42 -0300

    Projeto versão final (concluídos ajustes finais e organização do js)

--- scripts.js
+++ scripts.js
@@ -1,36 +1,34 @@
-let listaCards = [1,2,3,4,5,6,7];
-let pontuacao;
-let timer;
-let contadorClique;
+let temporizadorDiv = document.querySelector(".temporizador");
+let pontuacaoDiv = document.querySelector(".pontuacao");
+
+let baralho = [];
+
+let contadorClique = 0;
+let pontuacao = 0;
+let timer = 0;
+let idCartas = 0;
+
+let idIntervalCartas;
 let cronometro;
 let qdtCartas;
-let baralho;
 
 
-function jogar() {
-    setTimeout(function() {
-        
-        timer = 0;
-        pontuacao = 0;
-        baralho = [];
-        contadorClique = 0;
+setTimeout (escolherNumeroCartas,1000);
 
-        qdtCartas = prompt("Com quantas cartas deseja jogar? (4-14)");
-        qdtCartas = Number(qdtCartas);
+function escolherNumeroCartas() {
+    qdtCartas = Number(prompt("Com quantas cartas deseja jogar? (4-14)"));
 
-        while ((qdtCartas % 2) !== 0 || qdtCartas < 4 || qdtCartas > 14) {
-            qdtCartas = prompt("Por favor, insira um número par entre 4 e 14.");
-            qdtCartas = Number(qdtCartas);
-        }
+    while (ehImpar(qdtCartas) || ehValorInvalido(qdtCartas)) {
+        qdtCartas = Number(prompt("Por favor, insira um número par entre 4 e 14."));
+    }
 
-        atualizarBaralho();
-    }, 1000);
+    atualizarBaralho();
 }
-
-jogar();
 
 
 function atualizarBaralho() {
+    let listaCards = [1,2,3,4,5,6,7];
+
     listaCards.sort(embaralhar);
 
     for (let i = 0; i < (qdtCartas/2); i++) {
@@ -39,27 +37,39 @@ function atualizarBaralho() {
     }
     
     baralho.sort(embaralhar);
-
-    colocarCartas();
+    
+    idIntervalCartas  = setInterval(colocarCartas, 170);
 }
 
 
 function colocarCartas() {
 
-    for (let i = 0; i < qdtCartas; i++) {
-        
-        document.querySelector(".mesa").innerHTML += `
-        <div class="card" onclick="virarCard(this)">
-            <div class="back-face face">
-                <img src="./img/back-face.png">
-            </div>
-            <div class="front-face face">
-                <img src="./img/front-face/front-card-${baralho[i]}.gif">
-            </div>
-        </div>`;
+    document.querySelector(".mesa").innerHTML += `
+    <div class="card" onclick="virarCard(this)">
+        <div class="back-face face">
+            <img src="./img/back-face.png">
+        </div>
+        <div class="front-face face">
+            <img src="./img/front-face/front-card-${baralho[idCartas]}.gif">
+        </div>
+    </div>`;
+
+    idCartas++;
+
+    if (idCartas === qdtCartas) {
+        clearInterval(idIntervalCartas);
+
+        setTimeout(comecarJogo,500);
     }
 
-    cronometro = setInterval(temporizador, 1000);
+}
+
+
+function comecarJogo() {
+    temporizadorDiv.classList.add("comecar");
+    pontuacaoDiv.classList.add("comecar");
+    
+    cronometro = setInterval(atualizarTemporizador, 1000);
 }
 
 
@@ -72,84 +82,197 @@ function virarCard(carta) {
 
 
 function verificarCards() {
-    cartasViradas = document.querySelectorAll(".virar");
+    let cartasViradas = document.querySelectorAll(".virar");
 
-    if (cartasViradas.length === 2) {
+    if (ehPar(cartasViradas)) {
 
-        let card1 = cartasViradas.item(0).querySelector(".front-face img").src;
-        let card2 = cartasViradas.item(1).querySelector(".front-face img").src;
+        let card1 = cartasViradas.item(0);
+        let card2 = cartasViradas.item(1);
 
-        if (card1 === card2) {
-            cartasViradas.item(0).classList.add("parCerto");
-            cartasViradas.item(1).classList.add("parCerto");
+        if (ehParIgual(cartasViradas)) {
+            card1.classList.add("parCerto");
+            card2.classList.add("parCerto");
 
-            pontuacao += 100;
+            acrescentarPontuacao();
         } else {
-            pontuacao -= 10;
+            decrementarPontuacao();
         }
 
-        if (pontuacao < 0) {
-            pontuacao = 0;
-        }
+        setTimeout(atualizarPontuacao, 600);
 
-        document.querySelector(".pontuacao").innerHTML = `Pontuação final: ${pontuacao}`;
-
-        setTimeout(() => {
-            cartasViradas.item(0).classList.remove("virar");
-            cartasViradas.item(1).classList.remove("virar");
-        }, 1000);
+        setTimeout(validarPar, 1000);
 
         finalizarJogo();
     }
 }
 
 
-function finalizarJogo() {
-    let cartasCertas = document.querySelectorAll(".parCerto");
+function acrescentarPontuacao() {
+    pontuacao += 100;
+}
 
-    if (cartasCertas.length === qdtCartas) {
+
+function decrementarPontuacao() {
+    if ((pontuacao - 10) < 0) {
+        pontuacao = 0;
+    } else {
+        pontuacao -= 10;
+    }
+}
+
+
+function validarPar() {
+
+    let cartasViradas = document.querySelectorAll(".virar");
+    let card1 = cartasViradas.item(0);
+    let card2 = cartasViradas.item(1);
+
+    card1.classList.remove("virar");
+    card2.classList.remove("virar");
+}
+
+
+function finalizarJogo() {
+
+    if (ehTodasCertas()) {
 
         clearInterval(cronometro);
 
-        setTimeout(() => {
-            document.querySelector(".fim").classList.add("finish");
-        }, 500);
+        setTimeout(colocarTelaFim, 500);
 
-        setTimeout(() => {
-            alert(`Você ganhou em ${contadorClique} jogadas!\nTempo de partida: ${timer} segundos\nPontuação final: ${pontuacao} pontos`);
-        }, 700);
+        setTimeout(colocarAlertaFim, 700);
 
-        setTimeout(() => {
-            reiniciarPartida();
-        }, 1000);
+        setTimeout(reiniciarPartida, 1000);
     }
+}
+
+
+function colocarTelaFim() {
+    document.querySelector(".fim").classList.add("finish");
+}
+
+
+function colocarAlertaFim() {
+    alert(`Você ganhou em ${contadorClique} jogadas!
+    \nTempo de partida: ${timer} segundos\nPontuação: ${pontuacao} pontos`);
 }
 
 
 function reiniciarPartida() {
-    let reiniciar = prompt("Deseja jogar novamente? (sim/não)");
+    let reiniciar = '';
 
-    while (reiniciar !== "sim" && reiniciar !== "não") {
+    while (ehRespostaInvalida(reiniciar)) {
         reiniciar = prompt("Deseja jogar novamente? (sim/não)");
     }
 
     if (reiniciar === 'sim') {
-        document.querySelector(".mesa").innerHTML = '';
-        document.querySelector(".temporizador").innerHTML = 0;
-        document.querySelector(".pontuacao").innerHTML = `Pontuação final: 0`;
-        jogar();
+
+        limparMesa();
+        resetarVariaveis();
     }
 
+    retirarTelaFim();
+}
+
+
+function limparMesa() {
+    document.querySelector(".mesa").innerHTML = '';
+}
+
+
+function retirarTelaFim() {
     document.querySelector(".fim").classList.remove("finish");
 }
 
 
-function temporizador() {
+function resetarVariaveis() {
+
+    timer = 0;
+    baralho = [];
+    idCartas = 0;
+    pontuacao = 0;
+    qdtCartas = 0;
+    contadorClique = 0;
+
+    resetarTemporizador();
+    resetarPontuacao();
+
+    setTimeout (escolherNumeroCartas,1000);
+}
+
+
+function atualizarTemporizador() {
     timer++;
-    document.querySelector(".temporizador").innerHTML = timer;
+    temporizadorDiv.innerHTML = timer;
+}
+
+
+function atualizarPontuacao() {
+    pontuacaoDiv.innerHTML = `Pontuação: ${pontuacao}`;
+}
+
+
+function resetarTemporizador() {
+
+    temporizadorDiv.classList.remove("comecar");
+    temporizadorDiv.innerHTML = 0;
+}
+
+
+function resetarPontuacao() {
+
+    pontuacaoDiv.classList.remove("comecar");
+    pontuacaoDiv.innerHTML = `Pontuação: 0`;
 }
 
 
 function embaralhar() {
     return Math.random() - 0.5;
+}
+
+
+function ehImpar(qdtCartas) {
+    if ((qdtCartas % 2) !== 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function ehValorInvalido(qdtCartas) {
+    if (qdtCartas < 4 || qdtCartas > 14) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function ehPar(cartasViradas) {
+    return cartasViradas.length === 2;
+}
+
+
+function ehParIgual(cartasViradas) {
+    let card1 = cartasViradas.item(0).querySelector(".front-face img").src;
+    let card2 = cartasViradas.item(1).querySelector(".front-face img").src;
+
+    if (card1 === card2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function ehTodasCertas() {
+    let cartasCertas = document.querySelectorAll(".parCerto");
+
+    return cartasCertas.length === qdtCartas;
+}
+
+
+function ehRespostaInvalida(resposta) {
+    return resposta !== "sim" && resposta !== "não";
 }
